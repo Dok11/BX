9ff8941a7776401ec69d33841ab854a842221bef
Dok11 <mr.postoev@yandex.ru>
2017-01-05 22:38:25 +0300

    Dok\BX\Iblock. Добавил метод определения ID инфоблока по символьному коду

---
+++ tests/iblockTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dok\BX;
+
+class IbTest extends \PHPUnit_Framework_TestCase {
+	
+	/**
+	 * Получение инфоблока по коду
+	 * @dataProvider providerGetByCode
+	 */
+	public function testGetByCode($code, $res) {
+		$ib = new \Dok\BX\Iblock();
+		$this->assertEquals($res, $ib->getIblockByCode($code));
+		
+	}
+	
+	public function providerGetByCode() {
+		return Array(
+			Array('clothes',	2),
+			Array('false',		false),
+			Array(false,		false),
+			Array(0,			false),
+		);
+		
+	}
+	
+}
